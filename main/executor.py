c5f597d8a32244cf5244761148d4545fe4f7dc12
tdenzl <63013310+tdenzl@users.noreply.github.com>
2024-01-21 22:18:43 +0100

    adds more leagues

--- main/executor.py
+++ main/executor.py
@@ -3,6 +3,8 @@ from .scraper import Scraper
 class Executor:
 
     def execute(self):
-        scraper = Scraper("bundesliga", 21, 22, 1)
-        scraper.scrape()
-        scraper.store_data("GER1")
+        league_dict = {"bundesliga":"GER1", "premier-league":"ENG1", "la-liga":"ESP1", "serie-a":"ITA1", "ligue-1":"FRA1"}
+        for league, code in league_dict.items():
+            scraper = Scraper(league, 21, 22, 1)
+            scraper.scrape()
+            scraper.store_data(code)
